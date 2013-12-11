54ce4bcee3b02643d4c6d3a033328bbcde1365e4
pivotal <pivotal@fairfax.la.pivotallabs.com>
2013-12-10 20:11:06 -0800

    Generate Rails app (uses Rspec and Postgress)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecyclingPrices::Application.config.secret_key_base = 'd9b923e6a3c6f939deb1a4f6d74a0ff34d0f08e3850f9c7102d9a0cdaf9f5b8248a637cb8d1d795505f932dddbe058b6f059e1eaa72d6dd26ac112d7ba778dc4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RecyclingPrices::Application.config.session_store :cookie_store, key: '_recycling_prices_session'
