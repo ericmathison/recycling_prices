a9da58cc4197ca5da4a859a304c564403c726ba0
Eric Mathison and David Turner <dturn73@gmail.com>
2015-01-27 14:47:20 -0800

    Guard against nil error

--- spec/features/user_can_add_a_recycling_center_spec.rb
+++ spec/features/user_can_add_a_recycling_center_spec.rb
@@ -30,6 +30,30 @@ feature 'A user can add a recycling center' do
     end
   end
 
+  scenario 'by visiting the new center page' do
+    step 'and filling in all fileds except paper price' do
+      fill_in 'Name', with: "Al's Recycling"
+      fill_in 'Street', with: '123 Long Ln.'
+      fill_in 'City', with: 'Pasadena'
+      select 'California', from: 'State'
+      fill_in 'Zip', with: '91107'
+      fill_in 'Telephone', with: '626-123-1234'
+      fill_in 'Hours', with: '9 to 5'
+      fill_in 'Aluminum Price', with: '0.30'
+      fill_in 'Paper Price', with: ''
+      fill_in 'Plastic Price', with: '0.10'
+      fill_in 'Glass Price', with: '0.20'
+      click_on 'Save'
+    end
+
+    step 'takes me to the newly created record' do
+      ["Al's Recycling", '123 Long Ln.', 'Pasadena', 'CA', '91107',
+       '626-123-1234', '9 to 5', '0.30', '0.10', '0.20'].each do |text|
+        expect(page).to have_content(text)
+      end
+    end
+  end
+
   scenario 'unless appropriate fields are not filled out' do
     step "don't fill out any fields" do
       click_on 'Save'
